f7f27b8623706e651423ede42de9e133cbf4fe01
Codey <1307>
2023-11-27 23:37:34 +0200

    cards changes

--- scripts/classes/CardioVisit.js
+++ scripts/classes/CardioVisit.js
@@ -1,17 +1,29 @@
-import {getForm} from "../helpers/getForm.js";
+import { getForm } from "../helpers/getForm.js";
 import Visits from "./Visits.js";
 
 class CardioVisit extends Visits {
-    constructor({id, doctors, fullName, purpose, timing, description, status, pressure, index, diseases, age}) {
-        super({id, doctors, fullName, purpose, timing, description, status});
-        this.pressure = pressure;
-        this.index = index;
-        this.diseases = diseases;
-        this.age = age;
-    }
+  constructor({
+    id,
+    doctors,
+    fullName,
+    purpose,
+    timing,
+    description,
+    status,
+    pressure,
+    index,
+    diseases,
+    age,
+  }) {
+    super({ id, doctors, fullName, purpose, timing, description, status });
+    this.pressure = pressure;
+    this.index = index;
+    this.diseases = diseases;
+    this.age = age;
+  }
 
-    renderForm() {
-        const addValue = `
+  renderForm() {
+    const addValue = `
     <div class="lableVisit" id="formForDoctor">
     <lable id="pressure">Звичайний тиск:</lable>
     <input type="number" name="pressure" required></div>
@@ -24,40 +36,61 @@ class CardioVisit extends Visits {
     <div class="lableVisit">
     <lable id="age">Вік:</lable>
     <input type="number" name="age" required></div>
-    `
-        super.renderForm(addValue)
-        const option = document.getElementById('selectDoc');
-        console.log(option)
-        option.value = 'CardioVisit';
-        option.addEventListener("change", function (event) {
-                console.log(event.target.value)
-                getForm(event.target.value)
+    `;
+    super.renderForm(addValue);
+    const option = document.getElementById("selectDoc");
+    console.log(option);
+    option.value = "CardioVisit";
+    option.addEventListener("change", function (event) {
+      console.log(event.target.value);
+      getForm(event.target.value);
+    });
+  }
 
+  render() {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.id = this.id;
+    card.innerHTML = `<svg id="deleteBtn" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"/></svg>
+        <span class="cardInfo">${this.doctors}</span>
+        <span class="cardInfo">${this.fullName}</span>`;
 
-            }
-        )
-    }
+    const readMoreBtn = document.createElement("button");
+    readMoreBtn.innerText = "Показати більше";
+    readMoreBtn.classList.add("read_more_btn");
+    readMoreBtn.addEventListener("click", (e) => {
+      card.innerHTML = `
+          <svg id="deleteBtn" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"/></svg>
+        <span class="cardInfo">${this.doctors}</span>
+        <span class="cardInfo">${this.fullName}</span>
+        <span class="cardInfo">${this.description}</span>
+        <span class="cardInfo">${this.timing}</span>
+        <span class="cardInfo">${this.status}</span>
+        <span class="cardInfo">${this.pressure}</span>
+           <span class="cardInfo">${this.index}</span>
+          <span class="cardInfo">${this.diseases}</span>
+          <span class="cardInfo">${this.age}</span>`;
+      e.target.remove();
+    });
 
-    render() {
-        document.querySelector(".grid-container").insertAdjacentHTML('beforeend', `
-  <div class="card" id=${this.id}>
-  <svg id="deleteBtn" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"/></svg>
-  <span class="cardInfo">${this.doctors}</span>
-  <span class="cardInfo">${this.fullName}</span>
-  <a href="" class="read_more_btn">Показати більше</a>
-  <span class="cardInfo">${this.purpose}</span>
-  <span class="cardInfo">${this.description}</span>
-  <span class="cardInfo">${this.timing}</span>
-  <span class="cardInfo">${this.status}</span>
-  <span class="cardInfo">${this.pressure}</span>
-  <span class="cardInfo">${this.index}</span>
-  <span class="cardInfo">${this.diseases}</span>
-  <span class="cardInfo">${this.age}</span>
-  
-  
-  
-  </div>`)
-    }
+    card.append(readMoreBtn);
+    document.querySelector(".grid-container").prepend(card);
+    //         document.querySelector(".grid-container").insertAdjacentHTML('beforeend', `
+    //   <div class="card" id=${this.id}>
+    //   <svg id="deleteBtn" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"/></svg>
+    //   <span class="cardInfo">${this.doctors}</span>
+    //   <span class="cardInfo">${this.fullName}</span>
+    //   <a href="" class="read_more_btn">Показати більше</a>
+    //   <span class="cardInfo">${this.purpose}</span>
+    //   <span class="cardInfo">${this.description}</span>
+    //   <span class="cardInfo">${this.timing}</span>
+    //   <span class="cardInfo">${this.status}</span>
+    //   <span class="cardInfo">${this.pressure}</span>
+    //   <span class="cardInfo">${this.index}</span>
+    //   <span class="cardInfo">${this.diseases}</span>
+    //   <span class="cardInfo">${this.age}</span>
+    //   </div>`)
+  }
 }
 
 export default CardioVisit;
--- scripts/readMoreCards/readMoreCards.js
+++ scripts/readMoreCards/readMoreCards.js
@@ -1,15 +1,13 @@
 
 export function readMore(){
-// const hiddenCardInfo = document.querySelector(".card_hidden");
-
+const hiddenCardInfo = document.querySelector(".card_hidden");
 
 // const readMoreBtn = document.querySelector(".read_more_btn")
 // console.log(readMoreBtn)
 // readMoreBtn.addEventListener("click", (event) => {
 // hiddenCardInfo.style.display = 'block'
 // event.target.remove()
-// })
-
-console.log("123")
 }
+
+
 
